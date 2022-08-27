23ea5d45640bc80a5d82fe7a1c0ced8e7565e317
Fulerenn <maciej15s12@gmail.com>
2022-08-27 21:02:56 +0200

    First base prototype of ticktacktoe

---
+++ src/logic/ticktacktoe.ts
@@ -0,0 +1,15 @@
+enum Entity {
+    DEFAULT = 0,
+    CIRCLE = 1,
+    CROSS = 2,
+}
+
+enum GameState {
+    CIRCLE_MOVE = 1,
+    CROSS_MOVE = 2,
+
+    CIRCLE_WIN = 5,
+    CROSS_WIN = 6,
+
+    TIE = 7,
+}
