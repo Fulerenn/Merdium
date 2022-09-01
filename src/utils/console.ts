333c889a1b1b8a72280e047133b4b80db23bd845
Fulerenn <maciej15s12@gmail.com>
2022-09-01 21:55:39 +0200

    First prototype of console

---
+++ src/utils/console.ts
@@ -0,0 +1,38 @@
+//     Merdium - small single-server Discord bot
+//     Copyright (C) 2022  Fulerenn
+//
+//     This program is free software: you can redistribute it and/or modify
+//     it under the terms of the GNU General Public License as published by
+//     the Free Software Foundation, either version 3 of the License, or
+//     (at your option) any later version.
+//
+//     This program is distributed in the hope that it will be useful,
+//     but WITHOUT ANY WARRANTY; without even the implied warranty of
+//     MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+//     GNU General Public License for more details.
+//
+//     You should have received a copy of the GNU General Public License
+//     along with this program.  If not, see <https://www.gnu.org/licenses/>.
+
+class Console {
+    private content: string[];
+
+    constructor() {
+        this.content = ["Merdium Developer Console"];
+    }
+
+    println(content: string) {
+        this.content.push(content);
+    }
+
+    format() {
+        const clone = this.content;
+
+        clone.unshift("```");
+        clone.push("```");
+
+        return clone;
+    }
+}
+
+export default Console;
